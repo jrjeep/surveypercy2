ccc50e05599fd973deccb659f0089c77d3f15d3d
jrjeep <jrjeep@gmail.com>
2022-06-09 17:13:11 -0300

    Update firebase2.js

--- firebase2.js
+++ firebase2.js
@@ -1,15 +1,7 @@
 (function() {
     const config = {
 
-    /*  //Produção
-    apiKey: "AIzaSyDrQGvVqEEoM8sAtIC6DmKcu9N4dtic28g",
-    authDomain: "surveypercy-3914b.firebaseapp.com",
-    databaseURL: "https://surveypercy-3914b.firebaseio.com",
-    projectId: "surveypercy-3914b",
-    storageBucket: "surveypercy-3914b.appspot.com",
-    messagingSenderId: "748138801996"
- */	
- 	// TESTE
+
 	  apiKey: "AIzaSyDNzPAOYVVQshQAJLxIrPwx67Fi_6FmLNw",
 	  authDomain: "surveypercy-5ae68.firebaseapp.com",
 	  databaseURL: "https://surveypercy-5ae68-default-rtdb.firebaseio.com",
