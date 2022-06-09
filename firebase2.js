c83013acbbe1c79262f6054e2dbec511c074cada
jr <jrjeep@gmail.com>
2022-06-09 16:17:46 -0300

    initial commit

--- firebase2.js
+++ firebase2.js
@@ -1,13 +1,23 @@
 (function() {
     const config = {
-      //YOUR CONFIGS
+
+    /*  //Produção
     apiKey: "AIzaSyDrQGvVqEEoM8sAtIC6DmKcu9N4dtic28g",
     authDomain: "surveypercy-3914b.firebaseapp.com",
     databaseURL: "https://surveypercy-3914b.firebaseio.com",
     projectId: "surveypercy-3914b",
     storageBucket: "surveypercy-3914b.appspot.com",
     messagingSenderId: "748138801996"
-  
+ */	
+ 	// TESTE
+	  apiKey: "AIzaSyDNzPAOYVVQshQAJLxIrPwx67Fi_6FmLNw",
+	  authDomain: "surveypercy-5ae68.firebaseapp.com",
+	  databaseURL: "https://surveypercy-5ae68-default-rtdb.firebaseio.com",
+	  projectId: "surveypercy-5ae68",
+	  storageBucket: "surveypercy-5ae68.appspot.com",
+	  messagingSenderId: "80485888927",
+	  appId: "1:80485888927:web:75c75e2671c1d2d67083aa",
+	  measurementId: "G-0SWDLTTCCK"
     };
   
         // Draw the pie chart for Sarah's pizza when Charts is loaded.
@@ -23,7 +33,7 @@
   //  dbBigTextEvaluationStudentsRef.on('value', snap => bigTextEvaluationStudents.innerText = snap.val());
   
     var table = document.querySelector('#table1 tbody');
-    const dbEvaluationStudentsRef = firebase.database().ref('Questionarios').orderByChild('numPergunta').endAt('03');
+    const dbEvaluationStudentsRef = firebase.database().ref('Formularios/Quest_TESTE1').orderByChild('numPergunta').endAt('03');
     
     dbEvaluationStudentsRef.on('value', snap => {
       while(table.hasChildNodes()) {
@@ -40,7 +50,7 @@
                 ['pesquisador01', 0],
                 ['pesquisador02', 0],
                 ['pesquisador03', 0],
-                ['pesquisador04', 0]
+                ['jr - BASE DE TESTES!', 0]
             ];
             var outputArray01 = [
                 ['Grade', 'Bairros'],
@@ -61,14 +71,16 @@
                 ['10 - JARDIM JACARANDÁS / HABITAR BRASIL / JARDIM CELESTE / JARDIM ITAÚBAS', 0 ],
                 ['5 - RECANTO DOS BURITIS RESIDENCIAL / RESERVA CELESTE I, II, III / RESIDENCIAL SÃO FRANCISCO / RESIDENCIAL MORIA', 0 ],
                 ['8 - SÃO CRISTOVÃO / MENINO JESUS I, II / IPORÃ / PÉROLA / LISBOA / UMUARAMA I / VILLA ITALIA', 0 ],
-                ['10 - SEBASTIÃO DE MATOS I, II / UMUARAMA II / VILA MARIANA / VILA LOBOS / VILA SANTANA / VILA JULIANA / VILA AMÉRICA / BOA VISTA', 0 ]
+                ['10 - SEBASTIÃO DE MATOS I, II / UMUARAMA II / VILA MARIANA / VILA LOBOS / VILA SANTANA / VILA JULIANA / VILA AMÉRICA / BOA VISTA', 0 ],
+                ['Outros']
 
             ];
   
             var outputArray03 = [
                 ['Grade', 'Sexo'],
                 ['Masculino', 0],
-                ['Feminino', 0]
+                ['Feminino', 0],
+                ['Outro', 0]
             ];
 
   //pesquisador            
@@ -78,28 +90,31 @@
   var r0004 =0;
 
   //bairro
-  var r0101 =0;
-  var r0102 =0;
-  var r0103 =0;
-  var r0104 =0;
-  var r0105 =0;
-  var r0106 =0;
-  var r0107 =0;
-  var r0108 =0;
-  var r0109 =0;
-  var r0110 =0;
-  var r0111 =0;
-  var r0112 =0;
-  var r0113 =0;
-  var r0114 =0;
-  var r0115 =0;
-  var r0116 =0;
-  var r0117 =0;
-  var r0118 =0;
+  var r0201 =0;
+  var r0202 =0;
+  var r0203 =0;
+  var r0204 =0;
+  var r0205 =0;
+  var r0206 =0;
+  var r0207 =0;
+  var r0208 =0;
+  var r0209 =0;
+  var r0210 =0;
+  var r0211 =0;
+  var r0212 =0;
+  var r0213 =0;
+  var r0214 =0;
+  var r0215 =0;
+  var r0216 =0;
+  var r0217 =0;
+  var r0218 =0;
+  var r0299 =0;
+  
 
   //sexo
   var r0301 =0;
   var r0302 =0;
+  var r0399 =0;
 
 
      var x = 0;
@@ -127,93 +142,61 @@
                   } else if ((x==1) && ( students[i][j].slice(students[i][j].length-13, students[i][j].length) == 'pesquisador03')){ 
                       x = 0;
                       r0003++;			    				
-                  } else if ((x==1) && ( students[i][j].slice(students[i][j].length-13, students[i][j].length) == 'pesquisador04')){ 
+                  } else if ((x==1) && ( students[i][j].slice(students[i][j].length-2, students[i][j].length) == 'jr')){ 
                       x = 0;
                       r0004++;				
-                  } else if ((x==1) && (students[i][j] == '55 a 64')) { 
-                      x = 0;
-                      r0005++;
-                  } else if ((x==1) && (students[i][j] == '65 ou mais')) { 
-                      x = 0;
-                      r0006++;
                   }
 
 
 		  //bairro               
-                  if ((students[i][j] == '01')){ 
-                      x = 1  ;
-                  } 
-                  //if ((students[i][j] == '03') ){ 
-  
-                  if ((x==1) && (students[i][j] == '7 - CONDOMÍNIOS - MONDRIAN / PORTAL DA MATA / CARPE DIEM')){ 
-                      r0101++;		
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '5 - AQUARELA BRASIL / NOSSA SENHORA APARECIDA / JARDIM ITALIA / RESIDENCIAL FLORENÇA / AQUARELA DAS ARTES')) { 
-                      r0102++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '8 - DAURY RIVA / JARDIM VENEZA / JARDIM CALIFÓRNIA')) { 
-                      r0103++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '5 - SÃO PAULO I, II / MARIA VINDILINA I, II / RECANTO DOS PÁSSAROS')) { 
-                      r0104++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '10 - JARDIM BOA ESPERANÇA / JARDIM NOVO ESTADO / JARDIM DOS IPÊS')) { 
-                      r0105++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '5 - JARDIM EUROPA / RESIDENCIAL DELTA / RESIDENCIAL VITÓRIA RÉGIA')) { 
-                      r0106++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '11 - JARDIM DAS PALMEIRAS / JARDIM IMPERIAL')) { 
-                      r0107++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '15 - JARDIM JEQUITIBAS / JARDIM OLIVEIRAS / JARDIM AZALEIAS / JARDIM VIOLETAS')) { 
-                      r0108++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '4 - LOTEAMENTO FLORAIS DA AMAZÔNIA / RESIDENCIAL GENTE FELIZ I, II / JARDIM SAFIRA')) { 
-                      r0109++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '11 - JARIDM PRIMAVERAS / PARQUE DAS ARARAS / JARDIM DAS ORQUÍDEAS / RESIDENCIAL CANADA')) { 
-                      r0110++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '8 - RESIDENCIAL NORTE / SETOR COMERCIAL PARTE NORTE (AO NORTE DA JULIO CAMPOS)')) { 
-                      r0111++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '4 - JARDIM PARAÍSO II, III / JARDIM BELO HORIZONTE / RESIDENCIAL BELA SUIÇA')) { 
-                      r0112++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '8 - JARDIM BOTÂNICO / JARDIM PARAÍSO I')) { 
-                      r0113++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '6 - JARDIM SANTA MÔNICA / JARDIM IPIRANGA / RESIDENCIAL MARIPÁ / ADRIANO LEITÃO / JARDIM PAULISTA I, II / JARDIM IBIRAPUERA / PEQUENA LONDRES')) { 
-                      r0114++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '10 - JARDIM JACARANDÁS / HABITAR BRASIL / JARDIM CELESTE / JARDIM ITAÚBAS')) { 
-                      r0115++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '5 - RECANTO DOS BURITIS RESIDENCIAL / RESERVA CELESTE I, II, III / RESIDENCIAL SÃO FRANCISCO / RESIDENCIAL MORIA')) { 
-                      r0116++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '8 - SÃO CRISTOVÃO / MENINO JESUS I, II / IPORÃ / PÉROLA / LISBOA / UMUARAMA I / VILLA ITALIA')) { 
-                      r0117++;				
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == '10 - SEBASTIÃO DE MATOS I, II / UMUARAMA II / VILA MARIANA / VILA LOBOS / VILA SANTANA / VILA JULIANA / VILA AMÉRICA / BOA VISTA')) { 
-                      r0118++;				
-                      x = 0;
+                  if (students[i][j] == 'R0201'){ 
+                      r0201++;		
+                  } else if (students[i][j] == 'R0202') { 
+                      r0202++;				
+                  } else if (students[i][j] == 'R0203') { 
+                      r0203++;				
+                  } else if (students[i][j] == 'R0204') { 
+                      r0204++;				
+                  } else if (students[i][j] == 'R0205') { 
+                      r0205++;				
+                  } else if (students[i][j] == 'R0206') { 
+                      r0206++;				
+                  } else if (students[i][j] == 'R0207') { 
+                      r0207++;				
+                  } else if (students[i][j] == 'R0208') { 
+                      r0208++;				
+                  } else if (students[i][j] == 'R0209') { 
+                      r0209++;				
+                  } else if (students[i][j] == 'R0210') { 
+                      r0210++;				
+                  } else if (students[i][j] == 'R0211') { 
+                      r0211++;				
+                  } else if (students[i][j] == 'R0212') { 
+                      r0212++;				
+                  } else if (students[i][j] == 'R0213') { 
+                      r0213++;			
+                  } else if (students[i][j] == 'R0214') { 
+                      r0214++;				
+                  } else if (students[i][j] == 'R0215') { 
+                      r0215++;				
+                  } else if (students[i][j] == 'R0216') { 
+                      r0216++;				
+                  } else if (students[i][j] == 'R0217') { 
+                      r0217++;				
+                  } else if (students[i][j] == 'R0218') { 
+                      r0218++;				
+                  } else if (students[i][j] == 'R0299') { 
+                      r0299++;				
                   }
 
 
-		  //sexo                  
-                  if ((students[i][j] == '03')){ 
-                      x = 1  ;
-                  } 
-                  //if ((students[i][j] == '03') ){ 
-  
-                  if ((x==1) && (students[i][j] == 'Masculino')){ 
+		  //sexo                    
+                  if (students[i][j] == 'R0301'){ 
                       r0301++;		
-                      x = 0;
-                  } else if ((x==1) && (students[i][j] == 'Feminino')) { 
+                  } else if (students[i][j] == 'R0302') { 
                       r0302++;				
-                      x = 0;
+                  } else if (students[i][j] == 'R0399') { 
+                      r0399++;				
                   }
   
           }
@@ -227,25 +210,27 @@
   //sexo
   outputArray03[1][1]=r0301;
   outputArray03[2][1]=r0302;
+  outputArray03[3][1]=r0399;
   //Bairro
-  outputArray01[1][1]=r0101;
-  outputArray01[2][1]=r0102;
-  outputArray01[3][1]=r0103;
-  outputArray01[4][1]=r0104;
-  outputArray01[5][1]=r0105;
-  outputArray01[6][1]=r0106;
-  outputArray01[7][1]=r0107;
-  outputArray01[8][1]=r0108;
-  outputArray01[9][1]=r0109;
-  outputArray01[10][1]=r0110;
-  outputArray01[11][1]=r0111;
-  outputArray01[12][1]=r0112;
-  outputArray01[13][1]=r0113;
-  outputArray01[14][1]=r0114;
-  outputArray01[15][1]=r0115;
-  outputArray01[16][1]=r0116;
-  outputArray01[17][1]=r0117;
-  outputArray01[18][1]=r0118;
+  outputArray01[1][1]=r0201;
+  outputArray01[2][1]=r0202;
+  outputArray01[3][1]=r0203;
+  outputArray01[4][1]=r0204;
+  outputArray01[5][1]=r0205;
+  outputArray01[6][1]=r0206;
+  outputArray01[7][1]=r0207;
+  outputArray01[8][1]=r0208;
+  outputArray01[9][1]=r0209;
+  outputArray01[10][1]=r0210;
+  outputArray01[11][1]=r0211;
+  outputArray01[12][1]=r0212;
+  outputArray01[13][1]=r0213;
+  outputArray01[14][1]=r0214;
+  outputArray01[15][1]=r0215;
+  outputArray01[16][1]=r0216;
+  outputArray01[17][1]=r0217;
+  outputArray01[18][1]=r0218;
+  outputArray01[19][1]=r0218;
   
      //var total = 0;
      //for(var i in students) {
