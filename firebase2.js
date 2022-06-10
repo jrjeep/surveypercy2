2fec9eb3bbb7bc97392561a28704f7a72f9afdcf
jrjeep <jrjeep@gmail.com>
2022-06-10 18:56:41 -0300

    Update firebase2.js

--- firebase2.js
+++ firebase2.js
@@ -1,15 +1,11 @@
 (function() {
     const config = {
-
-
-	  apiKey: "AIzaSyDNzPAOYVVQshQAJLxIrPwx67Fi_6FmLNw",
-	  authDomain: "surveypercy-5ae68.firebaseapp.com",
-	  databaseURL: "https://surveypercy-5ae68-default-rtdb.firebaseio.com",
-	  projectId: "surveypercy-5ae68",
-	  storageBucket: "surveypercy-5ae68.appspot.com",
-	  messagingSenderId: "80485888927",
-	  appId: "1:80485888927:web:75c75e2671c1d2d67083aa",
-	  measurementId: "G-0SWDLTTCCK"
+    apiKey: "AIzaSyDrQGvVqEEoM8sAtIC6DmKcu9N4dtic28g",
+    authDomain: "surveypercy-3914b.firebaseapp.com",
+    databaseURL: "https://surveypercy-3914b.firebaseio.com",
+    projectId: "surveypercy-3914b",
+    storageBucket: "surveypercy-3914b.appspot.com",
+    messagingSenderId: "748138801996"
     };
   
         // Draw the pie chart for Sarah's pizza when Charts is loaded.
@@ -25,7 +21,7 @@
   //  dbBigTextEvaluationStudentsRef.on('value', snap => bigTextEvaluationStudents.innerText = snap.val());
   
     var table = document.querySelector('#table1 tbody');
-    const dbEvaluationStudentsRef = firebase.database().ref('Formularios/Quest_TESTE1').orderByChild('numPergunta').endAt('03');
+    const dbEvaluationStudentsRef = firebase.database().ref('Formularios/Quest_Juara').orderByChild('numPergunta').endAt('03');
     
     dbEvaluationStudentsRef.on('value', snap => {
       while(table.hasChildNodes()) {
