e63968487482bddc576238c1d01db3c3046347a6
jrjeep <jrjeep@gmail.com>
2022-06-11 20:59:34 -0300

    Update firebase2.js

--- firebase2.js
+++ firebase2.js
@@ -298,7 +298,7 @@
             title: 'idade',
             hAxis: {title: 'idade',  titleTextStyle: {color: '#333'}},
             vAxis: {minValue: 0},
-            colors: ['#008000', '#c6693e', '#cc8f6e', '#f3b49f', '#f6c7b6']
+            colors: ['#008000', '#4CFF33', '#337DFF', '#33FFE0', '#f6c7b6', '#AF33FF']
           };
   
           var chart = new google.visualization.PieChart(document.getElementById('bellChartDiv04'));
