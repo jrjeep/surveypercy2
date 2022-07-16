f7a603a7e05fe7132e93d64b617a24fa43d33fbe
jrjeep <jrjeep@gmail.com>
2022-07-15 21:33:06 -0300

    Update firebase2.js

--- firebase2.js
+++ firebase2.js
@@ -87,61 +87,19 @@
                 ['55 a 64', 0],
                 ['65 ou mais', 0]
             ];
-  //pesquisador            
-  var r0001 =0;
-  var r0002 =0;
-  var r0003 =0;
-  var r0004 =0;
-  var r0005 =0;
-  var r0006 =0;
-    //pesquisador            
-  var r9901 =0;
-  var r9902 =0;
-  var r9903 =0;
-  var r9904 =0;
-  var r9905 =0;
-  var r9906 =0;
+  //pesquisador 
+  var R00 = [0,0,0,0,0,0,0]           
+  //pesquisador ultima resposta            
+  var R99 = [0,0,0,0,0,0,0];
 
   //idade
-  var r0501 =0;
-  var r0502 =0;
-  var r0503 =0;
-  var r0504 =0;
-  var r0505 =0;
-  var r0506 =0;
+  var R05 = [0,0,0,0,0,0,0];
 
   //bairro
-  var r0301 =0;
-  var r0302 =0;
-  var r0303 =0;
-  var r0304 =0;
-  var r0305 =0;
-  var r0306 =0;
-  var r0307 =0;
-  var r0308 =0;
-  var r0309 =0;
-  var r0310 =0;
-  var r0311 =0;
-  var r0312 =0;
-  var r0313 =0;
-  var r0314 =0;
-  var r0315 =0;
-  var r0316 =0;
-  var r0317 =0;
-  var r0318 =0;
-  var r0319 =0;
-  var r0320 =0;
-  var r0321 =0;
-  var r0322 =0;
-  var r0323 =0;
-  
+  var R03 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
   //sexo
-  var R0401 =0;
-  var R0402 =0;
-  var R0403 =0;
-  var R0499 =0;
-
+  var R04 = [0,0,0,0,0];
 
      var x = 0, teste=0;
      var pesquisador = "";
@@ -162,206 +120,110 @@
   
                   if ((x==1) && ( students[i][j].slice(students[i][j].length-13, students[i][j].length) == 'pesquisador01')){ 
                       x = 0;
-			r9901 =1;
-			r9902 =0;
-			r9903 =0;
-			r9904 =0;
-			r9905 =0;
-			r9906 =0;
+                        var R99 = [0,1,0,0,0,0,0];
                   } else if ((x==1) && ( students[i][j].slice(students[i][j].length-13, students[i][j].length) == 'pesquisador02')){ 
                       x = 0;
-			r9901 =0;
-			r9902 =1;
-			r9903 =0;
-			r9904 =0;
-			r9905 =0;
-			r9906 =0;
+                        var R99 = [0,0,1,0,0,0,0];
                   } else if ((x==1) && ( students[i][j].slice(students[i][j].length-13, students[i][j].length) == 'pesquisador03')){ 
                       x = 0;
-			r9901 =0;
-			r9902 =0;
-			r9903 =1;
-			r9904 =0;
-			r9905 =0;
-			r9906 =0;
+                        var R99 = [0,0,0,1,0,0,0];
                   //} else if ((x==1) && ( students[i][j].slice(students[i][j].length-2, students[i][j].length) == 'jr')){ 
                   } else if ((x==1) && ( students[i][j].slice(students[i][j].length-13, students[i][j].length) == 'pesquisador04')){ 
                       x = 0;
-			r9901 =0;
-			r9902 =0;
-			r9903 =0;
-			r9904 =1;
-			r9905 =0;
-			r9906 =0;
-                                    
+                        var R99 = [0,0,0,0,1,0,0];
 		  //ultima pergunta            
                   } else if ((x==1) && ( students[i][j].slice(students[i][j].length-13, students[i][j].length) == 'pesquisador05')){ 
                       x = 0;
-			r9901 =0;
-			r9902 =0;
-			r9903 =0;
-			r9904 =0;
-			r9905 =1;
-			r9906 =0;                 
+                        var R99 = [0,0,0,0,0,1,0];
                   } else if ((x==1) && ( students[i][j].slice(students[i][j].length-13, students[i][j].length) == 'pesquisador06')){ 
                       x = 0;
-			r9901 =0;
-			r9902 =0;
-			r9903 =0;
-			r9904 =0;
-			r9905 =0;
-			r9906 =1;
+                        var R99 = [0,0,0,0,0,0,1];
                   }                  
                   if (students[i][j] == 'R3799'){ 
-                      r0001 = r0001 + r9901;		
-                      r0002 = r0002 + r9902;		
-                      r0003 = r0003 + r9903;		
-                      r0004 = r0004 + r9904;		
-                      r0005 = r0005 + r9905;		
-                      r0006 = r0006 + r9906;		
-			r9901 =0;
-			r9902 =0;
-			r9903 =0;
-			r9904 =0;
-			r9905 =0;
-			r9906 =0;
+                  	for(let i=1; i < R00.length; i++) {
+                  	  R00[i] = R00[i] + R99[i];
+                  	}
+                  	
+                  	  //sexo
+			  for (let i = 1; i < R04.length; i++)  {
+			  	outputArray03[i][1]=outputArray03[i][1]+R04[i];
+			  }  
+
+			  //idade
+			  for (let i = 1; i < R05.length; i++)  {
+			  	outputArray04[i][1]=outputArray04[i][1]+R05[i];
+			  }   
+			  
+			  //Bairro
+			  for (let i = 1; i < R03.length; i++)  {
+			  	outputArray01[i][1]=outputArray01[i][1]+R03[i];
+			  }
+                  	
+                  	//idade
+		        var R05 = [0,0,0,0,0,0,0];
+
+		        //bairro
+		        var R03 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+		        //sexo
+		        var R04 = [0,0,0,0,0];
+		        
+                      R99 = [0,0,0,0,0,0,0];
                   }  
                   
                   if (students[i][j].slice(0,5).toLowerCase() == 'teste'){ 
-			r9901 =0;
-			r9902 =0;
-			r9903 =0;
-			r9904 =0;
-			r9905 =0;
-			r9906 =0;
-			teste = 1; // teste verdadeiro 
+                      R99 = [0,0,0,0,0,0,0];
+		      teste = 1; // teste verdadeiro 
                   } 
                   
                if (teste == 0) {   
 
 		  //bairro               
-                  if (students[i][j] == 'R0301'){ 
-                      r0301++;		
-                  } else if (students[i][j] == 'R0302') { 
-                      r0302++;				
-                  } else if (students[i][j] == 'R0303') { 
-                      r0303++;				
-                  } else if (students[i][j] == 'R0304') { 
-                      r0304++;				
-                  } else if (students[i][j] == 'R0305') { 
-                      r0305++;				
-                  } else if (students[i][j] == 'R0306') { 
-                      r0306++;				
-                  } else if (students[i][j] == 'R0307') { 
-                      r0307++;				
-                  } else if (students[i][j] == 'R0308') { 
-                      r0308++;				
-                  } else if (students[i][j] == 'R0309') { 
-                      r0309++;				
-                  } else if (students[i][j] == 'R0310') { 
-                      r0310++;				
-                  } else if (students[i][j] == 'R0311') { 
-                      r0311++;				
-                  } else if (students[i][j] == 'R0312') { 
-                      r0312++;				
-                  } else if (students[i][j] == 'R0313') { 
-                      r0313++;				
-                  } else if (students[i][j] == 'R0314') { 
-                      r0314++;				
-                  } else if (students[i][j] == 'R0315') { 
-                      r0315++;				
-                  } else if (students[i][j] == 'R0316') { 
-                      r0316++;				
-                  } else if (students[i][j] == 'R0317') { 
-                      r0317++;				
-                  } else if (students[i][j] == 'R0318') { 
-                      r0318++;				
-                  } else if (students[i][j] == 'R0319') { 
-                      r0319++;				
-                  } else if (students[i][j] == 'R0320') { 
-                      r0320++;				
-                  } else if (students[i][j] == 'R0321') { 
-                      r0321++;				
-                  } else if (students[i][j] == 'R0322') { 
-                      r0322++;				
-                  } else if (students[i][j] == 'R0323') { 
-                      r0323++;				
+                  if (students[i][j].slice(0,3) == 'R03'){ 
+                      var indice03 = parseInt(students[i][j].slice(3,5))
+                      R03[indice03] = R03[indice03]+1;
                   }
 
 		  //sexo                    
-                  if (students[i][j] == 'R0401'){ 
-                      R0401++;		
-                  } else if (students[i][j] == 'R0402') { 
-                      R0402++;				
-                  } else if (students[i][j] == 'R0403') { 
-                      R0403++;				
-                  } else if (students[i][j] == 'R0499') { 
-                      R0499++;				
+                  if (students[i][j].slice(0,3) == 'R04'){ 
+                      var indice = parseInt(students[i][j].slice(3,5))
+                      //if indice = 99 {indice = 4}
+                      R04[indice] = R04[indice]+1;
                   }
                   
                   //idade               
-                  if (students[i][j] == 'R0501'){ 
-                      r0501++;		
-                  } else if (students[i][j] == 'R0502') { 
-                      r0502++;				
-                  } else if (students[i][j] == 'R0503') { 
-                      r0503++;				
-                  } else if (students[i][j] == 'R0504') { 
-                      r0504++;				
-                  } else if (students[i][j] == 'R0505') { 
-                      r0505++;				
-                  } else if (students[i][j] == 'R0506') { 
-                      r0506++;				
+                  if (students[i][j].slice(0,3) == 'R05'){ 
+                      var indice = parseInt(students[i][j].slice(3,5))
+                      R05[indice] = R05[indice]+1;
                   }
+
+                  
                }   
   
           }
           }
   //pesquisador
-  outputArray00[1][1]=r0001;
-  outputArray00[2][1]=r0002;
-  outputArray00[3][1]=r0003;
-  outputArray00[4][1]=r0004;
-  outputArray00[5][1]=r0005;
-  outputArray00[6][1]=r0006;
+  //outputArray00=R00;
+  for (let i = 1; i < R00.length; i++)  {
+  	outputArray00[i][1]=R00[i];
+  }
+  /* está fazendo acima, para filtrar formularios totalmente preenchidos
   //sexo
-  outputArray03[1][1]=R0401;
-  outputArray03[2][1]=R0402;
-  outputArray03[3][1]=R0403;
-  outputArray03[4][1]=R0499;
-  //idade
-  outputArray04[1][1]=r0501;
-  outputArray04[2][1]=r0502;
-  outputArray04[3][1]=r0503;
-  outputArray04[4][1]=r0504;
-  outputArray04[5][1]=r0505;
-  outputArray04[6][1]=r0506;
+  for (let i = 1; i < R04.length; i++)  {
+  	outputArray03[i][1]=R04[i];
+  }  
 
-  //Bairro
-  outputArray01[1][1]=r0301;
-  outputArray01[2][1]=r0302;
-  outputArray01[3][1]=r0303;
-  outputArray01[4][1]=r0304;
-  outputArray01[5][1]=r0305;
-  outputArray01[6][1]=r0306;
-  outputArray01[7][1]=r0307;
-  outputArray01[8][1]=r0308;
-  outputArray01[9][1]=r0309;
-  outputArray01[10][1]=r0310;
-  outputArray01[11][1]=r0311;
-  outputArray01[12][1]=r0312;
-  outputArray01[13][1]=r0313;
-  outputArray01[14][1]=r0314;
-  outputArray01[15][1]=r0315;
-  outputArray01[16][1]=r0316;
-  outputArray01[17][1]=r0317;
-  outputArray01[18][1]=r0318;
-  outputArray01[19][1]=r0319;
-  outputArray01[20][1]=r0320;
-  outputArray01[21][1]=r0321;
-  outputArray01[22][1]=r0322;
-  outputArray01[23][1]=r0323;
+  //idade
+  for (let i = 1; i < R05.length; i++)  {
+  	outputArray04[i][1]=R05[i];
+  }   
   
+  //Bairro
+  for (let i = 1; i < R03.length; i++)  {
+  	outputArray01[i][1]=R03[i];
+  }
+  */
+
      //var total = 0;
      //for(var i in students) {
         //var row = table.insertRow(-1);
