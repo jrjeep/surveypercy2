00ca56e5fae3cce88e20f3e09dec495fe3bdd721
jrjeep <jrjeep@gmail.com>
2023-06-09 15:48:35 -0300

    Update firebase2.js

--- firebase2.js
+++ firebase2.js
@@ -1,4 +1,4 @@
-var pesquisaName = 'Sinop_Midia_Abr_2023';
+var pesquisaName = 'Peixoto_Jun_2023';
 (function() {
     const config = {
     
@@ -8,11 +8,11 @@ var pesquisaName = 'Sinop_Midia_Abr_2023';
     projectId: "surveypercy-3914b",
     storageBucket: "surveypercy-3914b.appspot.com",
     messagingSenderId: "748138801996"
-
     };
     firebase.initializeApp(config);
     
-        // Load Charts and the corechart package.
+//    const pesquisaName = 'Sinop_Midia_Abr_2023';
+    // Load Charts and the corechart package.
         google.charts.load('current', {'packages':['corechart']});
     
     var table = document.querySelector('#table1 tbody');
@@ -42,45 +42,16 @@ var pesquisaName = 'Sinop_Midia_Abr_2023';
             ];
             var outputArray01 = [
                 ['Grade', 'Bairros'],
-                ['ALTO DA GLÓRIA I, II E III / CIDADE ALTA / JD VIENA / JD VILA BELLA / LIC SUL / JD ORIENTE / JD RIO PRETO / JD AMÉRICA', 0 ],
-                ['JD AURORA / SEBASTIÃO DE MATOS I E II / JD UMUARAMA II E III / JD ROMA / BOA VISTA / RESID SABRINA I, II E III / NOVO JD / CHÁC DE LAZER MONALIZA', 0 ],
-                ['VILLA REAL / BELA VILLE / PAINERAS / JD LIBERDADE / JD TROPICAL / BELA MORADA / MENINO JESUS I E II / VILLA ITÁLIA', 0 ],
-                ['COSTA RICA / PORTO RICO / LUXEMBURGO / MÓRIA / PANAMBY I E II / BURITIS / SÃO FRANCISCO / RESERVA CELESTE', 0 ],
-                ['BELVEDERE I E II / COND NICO BARACAT / JD MÁLAGA / VIDA NOVA / COND FLAMBOYANTS / BOM JD / CHÁC VITÓRIA / VILLA TOSCANA I E II / VILA TORINOS / BELVEDERE III / CANARINHO I E III / JAMARI', 0 ],
-                ['JD UMUARAMA / PQ TANGARÁ / JD ATENAS / JD LISBOA I E II / RESID PÉROLA / BAIRRO SÃO CRISTOVÃO / RESID IPIRANGA / RESID MARIPÁ / UIRAPURU / PEQUENA LONDRES / BOURBON RESID / RESID JARAGUÁ / RESID BONNE VIE / RESID SANTA CATARINA / JD SANTA MÔNICA', 0 ],
-                ['CIDADE NOVA / SONHALTO VIDEIRAS / VILLA VORATA / RAVENA / COMUNIDADE NOSSA SENHORA DE FÁTIMA / JD MILÃO I E II / QUATRO ESTAÇÕES BOULEVARD / MORUMBI', 0 ],
-                ['MONTE CARLO / JD SANTA ROSA / RESID PIENZA / RESID MONTREAL PARK / CHÁC DE LAZER SÃO CRISTÓVÃO I E II / JD PAULISTA I E II / JD IBIRAPUERA / RESID ADRIANO LEITÃO / RESID LONDRINA / JD NAPOLES / JD TULIPAS', 0 ],
-                ['JD ITAÚBAS (ZONA 07) / JD CURITIBA I E II / PLATINI URBAN PARK / RESID BOUNGAINVILLE / PQ AMAZONAS', 0 ],
-                ['HABITAR BRASIL / JOÃO PAULO II 1 E 2 / MATO GROSSO I E II / RESID CELESTE / JD CELESTE / JD DOS JACARANDÁS / RESID ALLEGRO', 0 ],
-                ['SETOR IND SUL / DISTRITO IND E COMERCIAL / SETOR IND / SETOR IND NORTE / RESID BRASÍLIA', 0 ],
-                ['JD BOTÂNICO / SETOR RESID SUL', 0 ],
-                ['TERRA RICA I E II / LEBLOM / JD COPACABANA', 0 ],
-                ['SÃO LOURENÇO I E II / JD DUBAI / COND TUIM RESID / VIVENDA DOS IPÊS / CIDADE JARDIM I E II / PORTAL DO SERVIDOR', 0 ],
-                ['RESID GOIABEIRAS / LIC NORTE / VILLA VERDE / COND RESID ECOVILLE / GENTE FELIZ I E II / JD IPORÃ', 0 ],
-                ['SAFIRA / VILA RICA / JD ITAPUÃ / FLORAIS DA AMAZÔNIA / JD TOLEDO / JD DOS JESUÍTAS / JD DO OURO', 0 ],
-                ['CAMPING CLUBE / JD PLANALTO / EIXO NORTE', 0 ],
-                ['SETOR COMERCIAL', 0 ],
-                ['JD PARAÍSO I, II E III / JD MARINGÁ I E II', 0 ],
-                ['BELO HORIZONTE / QUINTA DA PAMPULHA / BELA SUIÇA I, II E III', 0 ],
-                ['SETOR RESID NORTE / JD DAS PRIMAVERAS', 0 ],
-                ['PQ DAS ARARAS I E II / RESID CANADÁ / JD GRAMADO / JD DAS ORQUÍDEAS', 0 ],
-                ['JD DAS NAÇÕES I, II E III / NOSSA SENHORA APARECIDA I E II', 0 ],
-                ['JD IMPERIAL / JD DAS PALMEIRAS', 0 ],
-                ['RESID JEQUITIBÁS / JD DAS VIOLETAS', 0 ],
-                ['JD DAS OLIVEIRAS / JD DAS AZALÉIAS / JD MARIA CAROLINA / JD DOS CRAVOS', 0 ],
-                ['JD DAS ROSAS / KAIABI RESID / PRIVILLEGE RESID / RECANTO DA MATA / FLOR DO CARIBE RESID / JD CARIBE', 0 ],
-                ['JD VENEZA / DAURY RIVA / JD CALIFÓRNIA', 0 ],
-                ['MARIA VINDILINA I, II E III / COMUNIDADE BETEL / RECANTO DOS PÁSSAROS / JD TARUMÃS / JD DAS ACÁCIAS', 0 ],
-                ['RESID IGUATEMI / MOINHO DOS VENTOS / RESID ARAGUAIA / CAMPO VERDE', 0 ],
-                ['JD NOVO HORIZONTE / JD SÃO PAULO I E II / CARANDÁ BOSQUE / JD IPÊ / JD NOVO ESTADO / JD SANTA RITA', 0 ],
-                ['JD BOA ESPERANÇA / JD CONQUISTA', 0 ],
-                ['JD VITÓRIA RÉGIA / RESID DELTA / FIGUEIRAS RESID / JD EUROPA', 0 ],
-                ['LOTEAMENTO VILLAGE / ITÁLIA I, II E III / BARCELONA I E II', 0 ],
-                ['FLORENÇA / LIFE SINOP CLUB RESIDENCE / RESID SANTA CECÍLIA / RESID DEVILLE / ATIVA SAÚDE CENTER / IPANEMA', 0 ],
-                ['JD DE MONET / RECANTO SUIÇO / AQUARELA BRASIL RESID', 0 ],
-                ['COND MONDRIAN / COND PORTAL DA MATA / COND CARPE DIEM', 0 ],
-                ['RESID PARIS / AQUARELA DAS ARTES / ALAMEDA DAS CORES / AQUARELA DOS POEMAS', 0 ],
-                ['RESID PQ DO LAGO / RIVIERA SUÍÇA I E II / PORTINARI / SANTORINI RESIDENCE / SAN MARTINI / BAIRRO EUNICE / BAIRRO EUNICE - SÃO LUCAS', 0 ]
+                ['Centro Antigo', 0 ],
+                ['Centro Novo / Alvorada', 0 ],
+                ['Aeroporto', 0 ],
+                ['Mãe de Deus', 0 ],
+                ['Jerusalém', 0 ],
+                ['Liberdade', 0 ],
+                ['Nova Esperança', 0 ],
+                ['Santa Isabel', 0 ],
+                ['Bela Vista', 0 ],
+                ['União do Norte (zona rural)', 0 ]
 
             ];
   
@@ -103,11 +74,11 @@ var pesquisaName = 'Sinop_Midia_Abr_2023';
             
             var outputArray09 = [
                 ['Grade', 'Renda'],
-                ['MENOS DE R$ 1.302,00', 0],
-                ['R$ 1.303,00 A R$ 3.906,00 (1 A 3 S.M.)', 0],
-                ['R$ 3.907,00 A R$ 6.510,00 (3 A 5 S.M.)', 0],
-                ['R$ 6.511,00 A R$ 13.020,00 (5 A 10 S.M.)', 0],
-                ['R$ 13.021,00 ACIMA (MAIS DE 10 S.M.)', 0]
+                ['MENOS DE R$ 1.320,00', 0],
+                ['R$ 1.321,00 A R$ 3.960,00 (1 A 3 S.M.)', 0],
+                ['R$ 3.961,00 A R$ 6.600,00 (3 A 5 S.M.)', 0],
+                ['R$ 6.601,00 A R$ 13.200,00 (5 A 10 S.M.)', 0],
+                ['R$ 13.201,00 ACIMA (MAIS DE 10 S.M.)', 0]
             ];
             
             var outputArray07 = [
@@ -171,7 +142,7 @@ var pesquisaName = 'Sinop_Midia_Abr_2023';
   var R05 = [0,0,0,0,0,0,0];
 
   //bairro
-  var R03 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  var R03 = [0,0,0,0,0,0,0,0,0,0];
 
   //sexo
   var R04 = [0,0,0,0,0];
@@ -265,7 +236,7 @@ var pesquisaName = 'Sinop_Midia_Abr_2023';
                         var R99 = [0,0,0,0,0,0,0,0,0,0,0,0,0,1];
                         outputArray00[13][1]= students[i][j].slice(0, 3); 
                   }                  
-                  if (students[i][j] == 'R3199'){ 
+                  if (students[i][j] == 'R3899'){ 
                   	for(let i=1; i < R00.length; i++) {
                   	  R00[i] = R00[i] + R99[i];
                   	}
@@ -314,7 +285,7 @@ var pesquisaName = 'Sinop_Midia_Abr_2023';
 		        R05 = [0,0,0,0,0,0,0];
 
 		        //bairro
-		        R03 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+		        R03 = [0,0,0,0,0,0,0,0,0,0];
 
 		        //sexo
 		        R04 = [0,0,0,0,0];
@@ -347,7 +318,7 @@ var pesquisaName = 'Sinop_Midia_Abr_2023';
 
 		  //bairro               
                   if (students[i][j].slice(0,3) == 'R03'){ 
-                  	R03 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+                  	R03 = [0,0,0,0,0,0,0,0,0,0];
                       var indice03 = parseInt(students[i][j].slice(3,5))
                       R03[indice03] = R03[indice03]+1;
                   }
