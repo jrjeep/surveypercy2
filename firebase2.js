7bfb0c20bc177697d00802e9ceb50bee2cfb5b8c
jrjeep <jrjeep@gmail.com>
2023-12-05 15:24:26 -0300

    Update firebase2.js

--- firebase2.js
+++ firebase2.js
@@ -385,7 +385,7 @@ var sexo = 9
                   	}
                   	
 			  //Bairro
-			  for (let i = 1; i < 13; i++)  {
+			  for (let i = 1; i < outputArray03.length; i++)  {
 			  	outputArray03[i][1]=outputArray03[i][1]+R03[i];
 			  }
 
